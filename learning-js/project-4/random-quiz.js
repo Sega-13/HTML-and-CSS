4fc4b8a535ebcb9dbe53debe61837564dec9555e
Sega-13 <segapatidar@gmail.com>
2022-01-25 11:47:08 +0530

    Random Quiz using JS.

---
+++ learning-js/project-4/random-quiz.js
@@ -0,0 +1,23 @@
+//Array of object
+const quiz = [
+    {
+        q: "Sega's Bithdate ?",
+        options:[12, 13, 11, 9],
+        answer:1
+    },
+    {
+        q: "Sega's fav date ?",
+        options:[12, 13, 11, 9],
+        answer:3
+    },
+    {
+        q: "Sega's fav food ?", 
+        options:['chaat', 'pav-bhaji', 'pani-puri', 'dahi-puri'],
+        answer:2
+    },
+    {
+        q: "Sega's hobby ?" ,
+        options:['sleeping', 'shopping', 'singing', 'dancing'],
+        answer:0
+    }
+]
